6914e9f791492d05e65fb058015670d27e6f1f01
rick0000 <rickdev0000@gmail.com>
2020-09-15 07:15:40 +0900

    Fix command injection

--- mjaigym/tcp/server.py
+++ mjaigym/tcp/server.py
@@ -346,16 +346,29 @@ class Server:
                 server_game.start_game()
                 
 
+
+
+
             # manue room
             if len(self.waiting_rooms[room]) == 1 and 'manue' in room :
-                for i in range(3):
-                    thread = threading.Thread(target=self.run_manue, args=(room, f"manue{i}"), daemon=True)
-                    thread.start()
+                safe_room = True
+                # deny injection
+                invalid_chars = [";","|","&","`","(",")","$","<",">","*","?","{","}","[","]","|"]
+                for invalid_char in invalid_chars:
+                    if invalid_char in room:
+                        safe_room = False
+                        break
+
+                if safe_room:
+                    for i in range(3):
+                        thread = threading.Thread(target=self.run_manue, args=(room, f"manue{i}"), daemon=True)
+                        thread.start()
                     
 
     def run_manue(self, room_name, player_name):
+        
         proc = subprocess.run([
-                f"docker build -t manue:dev manue/. && docker run --rm manue:dev /mjai/mjai/run_manue.sh {player_name} {room_name}"
+                f"docker build -t manue:dev manue/. && docker run --rm manue:dev /mjai/mjai/run_manue.sh '{player_name}' '{room_name}'"
                 ], 
             shell=True,
             stdout=subprocess.DEVNULL,
